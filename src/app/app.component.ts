d82a9887973fa0e2be02c7573e706e7544ce3b29
Franco Pertusati <pertusatifranco836@gmail.com>
2024-07-15 15:34:46 -0300

    Nueva pagina 404 y diseño de rutas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { SidebarComponent } from "./components/sidebar/sidebar.component";
 import { NavbarComponent } from "./components/navbar/navbar.component";
 import { WelcomeComponent } from "./pages/welcome/welcome.component";
+import { LayoutComponent } from "./pages/layout/layout.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SidebarComponent, NavbarComponent, WelcomeComponent],
+  imports: [RouterOutlet, SidebarComponent, NavbarComponent, WelcomeComponent, LayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,22 @@
 import { Routes } from '@angular/router';
 import { BoardsComponent } from './pages/boards/boards.component';
+import { LayoutComponent } from './pages/layout/layout.component';
 import { BoardComponent } from './pages/board/board.component';
 import { WelcomeComponent } from './pages/welcome/welcome.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
       { path: '', component: WelcomeComponent },
       { path: 'boards', component: BoardsComponent },
       { path: 'boards/:id', component: BoardComponent },
+    ],
+  },
+  {
+    path: '**',
+    component: NotFoundComponent,
+  },
 ];
