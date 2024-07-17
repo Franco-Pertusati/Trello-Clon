69413733887e2f62bdff5c1c8053be11459e5099
Franco Pertusati <pertusatifranco836@gmail.com>
2024-07-17 10:39:34 -0300

    Ahora uso withHashLocation para manejar las rutas

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { provideRouter, withHashLocation, withRouterConfig } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { routes } from './app/app.routes';
 
 bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter(routes, withHashLocation())
-  ]
-}).catch(err => console.error(err));
+  providers: [provideRouter(routes, withHashLocation())],
+}).catch((err) => console.error(err));
